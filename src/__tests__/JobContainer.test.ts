d12b350223f633ca5efededb4bf4418fb93474c1
Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
2022-02-01 22:10:07 -0600

    Refactor store into different stores.

--- src/__tests__/JobContainer.test.ts
+++ src/__tests__/JobContainer.test.ts
@@ -3,12 +3,12 @@
  */
 
 import { render } from "@testing-library/svelte";
-import { _jobs } from "../store";
+import { jobs } from "../stores/job";
 
 import JobContainer from "../components/JobContainer.svelte";
 
 test("should contain Remote DevOps Engineer", () => {
-  const results = render(JobContainer, { props: { jobs: _jobs } });
+  const results = render(JobContainer, { props: { jobs } });
 
   expect(() => results.getByText("Remote DevOps Engineer")).not.toThrow();
 });
