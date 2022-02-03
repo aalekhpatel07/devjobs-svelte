3033c260d1d89e576e2b822192554bf47efd5a8d
Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
2022-02-02 20:03:30 -0600

    Add fall back for job details.

--- src/pages/Detail.svelte
+++ src/pages/Detail.svelte
@@ -1,33 +1,34 @@
 <script lang="ts">
-    import type { Job } from "../types/job";
     import { getJob } from "../stores/job";
     import Top from "../components/Detail/Top.svelte";
     import Body from "../components/Detail/Body.svelte";
     import Footer from "../components/Detail/Footer.svelte";
 
     export let jobId: number;
-    let job: Job;
 
 </script>
 
 <!-- <h2>{jobId}</h2> -->
 
-{#await getJob(jobId)}
-    We waiting for the job.
-{:then job}
-    <div class="mx-8 w-auto xs:max-w-4xl xs:mx-auto">
+<div class="mx-8 w-auto xs:max-w-4xl xs:mx-auto">
+    {#await getJob(jobId)}
+        <div class="min-h-screen -mt-6">
+            &nbsp;
+        </div>
+    {:then job}
         <div class="-mt-6">
             <Top job={job} />
         </div>
         <div class="mt-8">
             <Body job={job} />
         </div>
-    </div>
 
-    <div class="mt-16">
-        <Footer job={job} />
-    </div>
-{:catch}
-    <!-- <p>No job found</p> -->
-{/await}
-<!-- <h3>{job && Object.keys(job)}</h3> -->
+        <div class="mt-16">
+            <Footer job={job} />
+        </div>
+    {:catch}
+        <div class="min-h-screen -mt-6">
+            Could not find any job details for a job with id {jobId}.
+        </div>
+    {/await}
+</div>
