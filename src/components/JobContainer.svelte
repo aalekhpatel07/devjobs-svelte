26cbf74d8fecc5cd1520c5790c6e5150f2b94983
Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
2022-02-02 20:00:33 -0600

    Add fallback for no search results.

--- src/components/JobContainer.svelte
+++ src/components/JobContainer.svelte
@@ -3,18 +3,25 @@
 
 	import Job from './Job.svelte';
 	import type { Job as JobType } from '../types/job';
+    import { searchQuery } from '../stores/job';
 
     export let jobs: JobType[];
 
 </script>
+{#if jobs.length === 0}
+    <div class="mx-auto w-4/5 text-center pt-16 text-dark-grey">
+        <p>No jobs found for "{$searchQuery}".</p>
+    </div>
+{:else}
+    <ul class="mx-auto grid grid-cols-auto px-4 pt-16 pb-8 gap-16 justify-items-center">
+        {#each jobs as job (job.id)}
+            <li transition:fade|local="{{ duration: 200 }}">
+                <Job {job}/>
+            </li>
+        {/each}
+    </ul>
+{/if}
 
-<ul class="mx-auto grid grid-cols-auto px-4 pt-16 pb-8 gap-16 justify-items-center">
-    {#each jobs as job (job.id)}
-        <li transition:fade|local="{{ duration: 200 }}">
-            <Job {job}/>
-        </li>
-    {/each}
-</ul>
 
 <style>
 	.grid-cols-auto {
